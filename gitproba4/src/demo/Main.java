3af2b0f8e33bb39759a933017d8f5adc5780fdce
Marius Trusca <MTrusca@luxoft.com>
2018-01-29 13:44:52 +0200

    a doua schimbare

--- gitproba4/src/demo/Main.java
+++ gitproba4/src/demo/Main.java
@@ -15,6 +15,7 @@ public class Main {
 	public static void  metoda2() {
 		System.out.println("sunt in metoda din clasa main");
 		System.out.println("am mai adaugat cevadmariusss"+ "facem o schimbare mica");
+		System.out.println("a doua schimbare");
 	}
 
 }
