1119664788d3545031813926edd56b0ec32bbd04
Marius Trusca <MTrusca@luxoft.com>
2018-01-29 13:31:23 +0200

    initial classes

---
+++ gitproba4/src/demo/Main.java
@@ -0,0 +1,20 @@
+package demo;
+
+public class Main {
+
+	
+	
+	
+	public static void main(String[] args) {
+		
+		
+		Ex1.metoda1();
+		metoda2();
+	}
+	
+	public static void  metoda2() {
+		System.out.println("sunt in metoda din clasa main");
+		System.out.println("am mai adaugat cevadmariusss");
+	}
+
+}
