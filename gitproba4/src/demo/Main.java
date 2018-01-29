42d99acc8d3a3373ce9a5bf33f368beea8b64d89
Marius Trusca <MTrusca@luxoft.com>
2018-01-29 13:36:39 +0200

    de fapt e prima schimbare

--- gitproba4/src/demo/Main.java
+++ gitproba4/src/demo/Main.java
@@ -14,7 +14,7 @@ public class Main {
 	
 	public static void  metoda2() {
 		System.out.println("sunt in metoda din clasa main");
-		System.out.println("am mai adaugat cevadmariusss");
+		System.out.println("am mai adaugat cevadmariusss"+ "facem o schimbare mica");
 	}
 
 }
